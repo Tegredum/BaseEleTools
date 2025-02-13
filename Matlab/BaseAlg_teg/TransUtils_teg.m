e9b3e3627ab309e13bb61ca4aa3b2bce051a3701
Tegredum <3257087065@qq.com>
2025-02-13 21:27:23 +0800

    feat & chores: 补充从速度大小与方向计算速度矢量的算法，并给出其偏导数算法，另外形成了文档

---
+++ Matlab/BaseAlg_teg/TransUtils_teg.m
@@ -0,0 +1,87 @@
+% encoding: utf-8
+% author: Tegredum
+% MATLAB version: R2024b
+% 提供若干常用变换算法接口（三维速度矢量与速度大小方向间的变换、坐标系变换等）
+classdef TransUtils_teg
+	methods(Static)
+		% 将速度大小、方向转换为速度矢量，遵循北天东定义
+		function velVec = velDir_2_velVec(inArgs)
+			arguments(Input)
+				inArgs.V		(1, 1)	double	% 速度大小
+				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
+				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+			end
+			arguments(Output)
+				velVec	(3, 1)	double	% 速度矢量
+			end
+			theta = inArgs.theta;
+			psi = inArgs.psi;
+			ctheta = cos(theta);
+			stheta = sin(theta);
+			cpsi = cos(psi);
+			spsi = sin(psi);
+			velVec = [
+				ctheta * cpsi;
+				stheta;
+				-ctheta * spsi
+			] * inArgs.V;
+		end
+		% 按照从速度大小与方向转换至速度矢量的公式，计算速度矢量关于速度大小的偏导数。原理公式见文档
+		function result = par_velVec_par_V(inArgs)
+			arguments(Input)
+				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
+				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+			end
+			arguments(Output)
+				result	(3, 1)	double	% 速度矢量关于速度大小的偏导数
+			end
+			result = velDir_2_velVec(...
+				'V', 1.0,...
+				'theta', inArgs.theta,...
+				'psi', inArgs.psi);
+		end
+		% 按照从速度大小与方向转换至速度矢量的公式，计算速度矢量关于弹道倾角的偏导数。原理公式见文档
+		function result = par_velVec_par_theta(inArgs)
+			arguments(Input)
+				inArgs.V		(1, 1)	double	% 速度大小
+				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
+				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+			end
+			arguments(Output)
+				result	(3, 1)	double	% 速度矢量关于弹道倾角的偏导数
+			end
+			theta = inArgs.theta;
+			psi = inArgs.psi;
+			ctheta = cos(theta);
+			stheta = sin(theta);
+			cpsi = cos(psi);
+			spsi = sin(psi);
+			result = [
+				-stheta * cpsi;
+				ctheta;
+				stheta * spsi
+			] * inArgs.V;
+		end
+		% 按照从速度大小与方向转换至速度矢量的公式，计算速度矢量关于弹道偏角的偏导数。原理公式见文档
+		function result = par_velVec_par_psi(inArgs)
+			arguments(Input)
+				inArgs.V		(1, 1)	double	% 速度大小
+				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
+				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+			end
+			arguments(Output)
+				result	(3, 1)	double	% 速度矢量关于弹道偏角的偏导数
+			end
+			theta = inArgs.theta;
+			psi = inArgs.psi;
+			ctheta = cos(theta);
+			cpsi = cos(psi);
+			spsi = sin(psi);
+			result = [
+				spsi;
+				0.0;
+				cpsi
+			] * (-inArgs.V * ctheta);
+		end
+	end
+end

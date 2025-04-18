fb81d629c3de625721dac938f42e9d97251428c6
Tegredum <3257087065@qq.com>
2025-04-18 16:11:21 +0800

    fix: 将 MATLAB 侧 TransUtils 中原用于分析速度矢量与速度大小、方向角之间关系的程序扩展至三维空间矢量，使这部分程序可以套用于视线角计算

--- Matlab/BaseAlg_teg/TransUtils_teg.m
+++ Matlab/BaseAlg_teg/TransUtils_teg.m
@@ -4,15 +4,15 @@
 % 提供若干常用变换算法接口（三维速度矢量与速度大小方向间的变换、坐标系变换等）
 classdef TransUtils_teg
 	methods(Static)
-		% 将速度大小、方向转换为速度矢量，遵循北天东定义
-		function velVec = velDir_2_velVec(inArgs)
+		% 将矢量大小、方向转换为矢量本身，遵循北天东定义
+		function vec = normDir_2_vec(inArgs)
 			arguments(Input)
-				inArgs.V		(1, 1)	double	% 速度大小
-				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
-				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+				inArgs.norm		(1, 1)	double	% 矢量大小
+				inArgs.theta	(1, 1)	double	% 矢量倾角（rad）
+				inArgs.psi		(1, 1)	double	% 矢量偏角（rad）
 			end
 			arguments(Output)
-				velVec	(3, 1)	double	% 速度矢量
+				vec		(3, 1)	double	% 矢量本身
 			end
 			theta = inArgs.theta;
 			psi = inArgs.psi;
@@ -20,59 +20,59 @@ classdef TransUtils_teg
 			stheta = sin(theta);
 			cpsi = cos(psi);
 			spsi = sin(psi);
-			velVec = [
+			vec = [
 				ctheta * cpsi;
 				stheta;
 				-ctheta * spsi
-			] * inArgs.V;
+			] * inArgs.norm;
 		end
-		% 将速度矢量转换为速度大小、方向，遵循北天东定义
-		function outArgs = velVec_2_velDir(inVec)
+		% 将矢量本身转换为矢量大小、方向，遵循北天东定义
+		function outArgs = vec_2_normDir(inVec)
 			arguments
-				inVec	(3, 1)	double	% 输入速度矢量
+				inVec	(3, 1)	double	% 输入矢量本身
 			end
-			V = norm(inVec);
-			outArgs = struct('V', V);
-			if V == 0
+			r = norm(inVec);
+			outArgs = struct('norm', norm);
+			if norm == 0
 				outArgs.status = 'zeroVec';
 				return
 			end
-			vy = inVec(2);
-			theta = asin(vy / V);
+			ry = inVec(2);
+			theta = asin(ry / r);
 			outArgs.theta = theta;
-			vx = inVec(1);
-			vz = inVec(3);
-			if vx == 0 && vz == 0
+			rx = inVec(1);
+			rz = inVec(3);
+			if rx == 0 && rz == 0
 				outArgs.status = 'verticalVec';
 				return
 			end
-			psi = atan2(-vz, vx);
+			psi = atan2(-rz, rx);
 			outArgs.psi = psi;
 			outArgs.status = 'ok';
 		end
-		% 按照从速度大小与方向转换至速度矢量的公式，计算速度矢量关于速度大小的偏导数。原理公式见文档
-		function result = par_velVec_par_V(inArgs)
+		% 按照从矢量大小与方向转换至矢量本身的公式，计算矢量关于矢量大小的偏导数。原理公式见文档
+		function result = par_vec_par_norm(inArgs)
 			arguments(Input)
-				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
-				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+				inArgs.theta	(1, 1)	double	% 矢量倾角（rad）
+				inArgs.psi		(1, 1)	double	% 矢量偏角（rad）
 			end
 			arguments(Output)
-				result	(3, 1)	double	% 速度矢量关于速度大小的偏导数
+				result	(3, 1)	double	% 矢量本身关于矢量大小的偏导数
 			end
-			result = TransUtils_teg.velDir_2_velVec(...
-				'V', 1.0,...
+			result = TransUtils_teg.normDir_2_vec(...
+				'norm', 1.0,...
 				'theta', inArgs.theta,...
 				'psi', inArgs.psi);
 		end
-		% 按照从速度大小与方向转换至速度矢量的公式，计算速度矢量关于弹道倾角的偏导数。原理公式见文档
-		function result = par_velVec_par_theta(inArgs)
+		% 按照从矢量大小与方向转换至矢量本身的公式，计算矢量关于矢量倾角的偏导数。原理公式见文档
+		function result = par_vec_par_theta(inArgs)
 			arguments(Input)
-				inArgs.V		(1, 1)	double	% 速度大小
-				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
-				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+				inArgs.norm		(1, 1)	double	% 矢量大小
+				inArgs.theta	(1, 1)	double	% 矢量倾角（rad）
+				inArgs.psi		(1, 1)	double	% 矢量偏角（rad）
 			end
 			arguments(Output)
-				result	(3, 1)	double	% 速度矢量关于弹道倾角的偏导数
+				result	(3, 1)	double	% 矢量本身关于矢量倾角的偏导数
 			end
 			theta = inArgs.theta;
 			psi = inArgs.psi;
@@ -84,17 +84,17 @@ classdef TransUtils_teg
 				-stheta * cpsi;
 				ctheta;
 				stheta * spsi
-			] * inArgs.V;
+			] * inArgs.norm;
 		end
-		% 按照从速度大小与方向转换至速度矢量的公式，计算速度矢量关于弹道偏角的偏导数。原理公式见文档
-		function result = par_velVec_par_psi(inArgs)
+		% 按照从矢量大小与方向转换至矢量本身的公式，计算矢量关于矢量偏角的偏导数。原理公式见文档
+		function result = par_vec_par_psi(inArgs)
 			arguments(Input)
-				inArgs.V		(1, 1)	double	% 速度大小
-				inArgs.theta	(1, 1)	double	% 弹道倾角（rad）
-				inArgs.psi		(1, 1)	double	% 弹道偏角（rad）
+				inArgs.norm		(1, 1)	double	% 矢量大小
+				inArgs.theta	(1, 1)	double	% 矢量倾角（rad）
+				inArgs.psi		(1, 1)	double	% 矢量偏角（rad）
 			end
 			arguments(Output)
-				result	(3, 1)	double	% 速度矢量关于弹道偏角的偏导数
+				result	(3, 1)	double	% 矢量本身关于矢量偏角的偏导数
 			end
 			theta = inArgs.theta;
 			psi = inArgs.psi;
@@ -105,7 +105,7 @@ classdef TransUtils_teg
 				spsi;
 				0.0;
 				cpsi
-			] * (-inArgs.V * ctheta);
+			] * (-inArgs.norm * ctheta);
 		end
 	end
 end
